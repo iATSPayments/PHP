b4473e3d09ef751fcbee697010ef4b1f2599bb5b
ruscoe <danruscoe@gmail.com>
2014-02-10 13:42:47 -0800

    Populated Delete Customer Code method.

--- lib/CustomerLink.php
+++ lib/CustomerLink.php
@@ -185,12 +185,71 @@ class CustomerLink extends Core {
     return $this->responseHandler($response, 'CreateACHEFTCustomerCodeV1Result');
   }
 
+  /**
+   * Update ACH/EFT Customer Code.
+   *
+   * ACH: http://en.wikipedia.org/wiki/Automated_Clearing_House
+   * EFT: http://en.wikipedia.org/wiki/Electronic_funds_transfer
+   *
+   * @param array $parameters
+   *   An associative array with the following possible values.
+   *
+   * @code
+   *   array(
+   *     'customerIPAddress' => '',
+   *     'customerCode' => '',
+   *     'firstName' => 'Test',
+   *     'lastName' => 'Account',
+   *     'companyName' => 'Test Co.',
+   *     'address' => '1234 Any Street',
+   *     'city' => 'Schenectady',
+   *     'state' => 'NY',
+   *     'zipCode' => '12345',
+   *     'phone' => '555-555-1234',
+   *     'fax' => '555-555-4321',
+   *     'alternatePhone' => '555-555-5555',
+   *     'email' => 'email@test.co',
+   *     'comment' => 'Customer code update test.',
+   *     'recurring' => FALSE,
+   *     'amount' => '5',
+   *     'beginDate' => 946684800,
+   *     'endDate' => 946771200,
+   *     'scheduleType' => 'Annually',
+   *     'scheduleDate' => '',
+   *     'accountCustomerName' => 'Test Account',
+   *     'accountNum' => '999999999',
+   *     'accountType' => 'Checking',
+   *     'updateAccountNum' => FALSE,
+   *   );
+   * @endcode
+   *
+   * @return mixed
+   *   Client response array or API error.
+   */
   public function updateACHEFTCustomerCode($parameters) {
-    // TODO: Populate method.
+    $response = $this->apiCall('UpdateACHEFTCustomerCode', $parameters);
+    return $this->responseHandler($response, 'UpdateACHEFTCustomerCodeV1Response');
   }
 
+  /**
+   * Delete Customer Code.
+   *
+   * @param array $parameters
+   *   An associative array with the following possible values.
+   *
+   * @code
+   *   array(
+   *     'customerIPAddress' => '',
+   *     'customerCode' => '',
+   *   );
+   * @endcode
+   *
+   * @return mixed
+   *   Client response array or API error.
+   */
   public function deleteCustomerCode($parameters) {
-    // TODO: Populate method.
+    $response = $this->apiCall('DeleteCustomerCode', $parameters);
+    return $this->responseHandler($response, 'DeleteCustomerCodeV1Response');
   }
 
   /**
