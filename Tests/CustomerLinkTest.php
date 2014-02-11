1edb70ddec93040ce81ff318fc6fb7dee4cb418e
Dan Ruscoe <danruscoe@gmail.com>
2014-02-11 09:52:38 -0800

    Added unit test for updating recurring payment schedule type / date.

--- Tests/CustomerLinkTest.php
+++ Tests/CustomerLinkTest.php
@@ -165,6 +165,64 @@ class CustomerLinkTest extends \PHPUnit_Framework_TestCase {
   }
 
   /**
+   * Test updateCreditCardCustomerCode by updating the recurring schedule date.
+   *
+   * @depends testCustomerLinkupdateCreditCardCustomerCodeNewRecurringSchedule
+   */
+  public function testCustomerLinkupdateCreditCardCustomerCodeNewRecurringScheduleDate() {
+    $agentcode = self::AGENT_CODE;
+    $password = self::PASSWORD;
+    $beginDate = strtotime('10/23/2011');
+    $endDate = strtotime('10/23/2014');
+    // Create and populate the request object.
+    $request = array(
+      'customerIPAddress' => '',
+      'customerCode' => self::TEST_CREDIT_CARD_CUSTOMER_CODE,
+      'firstName' => 'Test',
+      'lastName' => 'Account',
+      'companyName' => 'Test Co.',
+      'address' => '1234 Any Street',
+      'city' => 'Schenectady',
+      'state' => 'NY',
+      'zipCode' => '12345',
+      'phone' => '555-555-1234',
+      'fax' => '555-555-4321',
+      'alternatePhone' => '555-555-5555',
+      'email' => 'email@test.co',
+      'comment' => 'Customer code update test.',
+      'recurring' => TRUE,
+      'amount' => '5',
+      'beginDate' => $beginDate,
+      'endDate' => $endDate,
+      'scheduleType' => 'Weekly',
+      'scheduleDate' => '7', // 7th day of the week.
+      'creditCardCustomerName' => 'Test Account',
+      'creditCardNum' => '4222222222222220',
+      'creditCardExpiry' => '12/17',
+      'mop' => 'VISA',
+      'updateCreditCardNum' => FALSE,
+    );
+
+    $iats = new CustomerLink($agentcode, $password, 'NA');
+    $response = $iats->updateCreditCardCustomerCode($request);
+    $this->assertEquals('OK', $response);
+
+    // Get customer details to confirm update.
+
+    $request = array(
+      'customerIPAddress' => '',
+      'customerCode' => self::TEST_CREDIT_CARD_CUSTOMER_CODE,
+    );
+
+    $response = $iats->getCustomerCodeDetail($request);
+
+    $this->assertArrayHasKey('CST', $response);
+
+    $this->assertEquals('Weekly', $response['CST']['RCR']['SCHTYP']);
+    $this->assertEquals(7, $response['CST']['RCR']['SCHD']);
+  }
+
+  /**
    * Test getCustomerCodeDetail.
    *
    * @depends testCustomerLinkupdateCreditCardCustomerCode
@@ -328,12 +386,6 @@ class CustomerLinkTest extends \PHPUnit_Framework_TestCase {
 
 
 
-//  /**
-//   * Change recurring schedule date.
-//   */
-//  public function testRecurDate() {
-//    $this->assertTrue(FALSE);
-//  }
 //
 //  /**
 //   * Change recurring schedule frequency.
