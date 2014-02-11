616b873e7c13c6feb55efaf14ff490ca45901503
Dan Ruscoe <danruscoe@gmail.com>
2014-02-11 15:22:12 -0800

    Added unit test for create customer and process ACH / EFT method. Added variables shared between test methods.

--- Tests/ProcessLinkTest.php
+++ Tests/ProcessLinkTest.php
@@ -15,11 +15,48 @@ class ProcessLinkTest extends \PHPUnit_Framework_TestCase {
   const AGENT_CODE = 'TEST88';
   const PASSWORD = 'TEST88';
 
+  /** @var string $ACHEFTCustomerCode */
+  private $ACHEFTCustomerCode;
+
+  /** @var string $ACHEFTTransationId */
+  private $ACHEFTTransationId;
+
+  /** @var string $creditCardCustomerCode */
+  private $creditCardCustomerCode;
+
+  /** @var string $creditCardTransactionId */
+  private $creditCardTransactionId;
+
   /**
    * Test createCustomerCodeAndProcessACHEFT.
    */
   public function testProcessLinkcreateCustomerCodeAndProcessACHEFT() {
+    $agentcode = self::AGENT_CODE;
+    $password = self::PASSWORD;
 
+    // Create and populate the request object.
+    $request = array(
+      'customerIPAddress' => '',
+      'firstName' => 'Test',
+      'lastName' => 'Account',
+      'address' => '1234 Any Street',
+      'city' => 'Schenectady',
+      'state' => 'NY',
+      'zipCode' => '12345',
+      'accountNum' => '02100002100000000000000001',
+      'accountType' => 'CHECKING',
+      'invoiceNum' => '00000001',
+      'total' => '5',
+      'comment' => 'Process direct debit test.',
+    );
+
+    $iats = new ProcessLink($agentcode, $password);
+    $response = $iats->createCustomerCodeAndProcessACHEFT($request);
+
+    $this->creditCardCustomerCode = trim($response['PROCESSRESULT']['CUSTOMERCODE']);
+    $this->creditCardTransactionId = trim($response['PROCESSRESULT']['TRANSACTIONID']);
+
+    $this->assertEquals('Success', $response['STATUS']);
   }
 
   /**
@@ -113,7 +150,7 @@ class ProcessLinkTest extends \PHPUnit_Framework_TestCase {
     $iats = new ProcessLink($agentcode, $password);
     $response = $iats->processCreditCard($request);
 
-    $this->assertEquals('SUCCESS', $response['STATUS']);
+    $this->assertEquals('Success', $response['STATUS']);
 
     $clean = trim($response['PROCESSRESULT']['AUTHORIZATIONRESULT']);
     $this->assertEquals($clean, 'OK: 678594:');
