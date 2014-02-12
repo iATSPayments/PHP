c4d88dce163c51a7e39134d3bfd69d6cd7512706
Dan Ruscoe <danruscoe@gmail.com>
2014-02-12 11:47:24 -0800

    MOP currency restriction check now restricts transactions using wrong currency for a server.

--- lib/Core.php
+++ lib/Core.php
@@ -213,17 +213,24 @@ class Core {
     $restricted = FALSE;
     $matrix = $this->getMOPCurrencyMatrix();
     if (isset($matrix[$serverid][$currency])) {
+      // Validate Method of Payment (MOP) against currency.
       $filterresult = array_filter($matrix[$serverid][$currency],
       function ($item) use ($mop) {
         return $item == $mop;
       }
       );
       if (empty($filterresult)) {
+        // Currency not valid for any Methods of Payment.
         $restricted = TRUE;
       }
       else {
         $restricted = FALSE;
       }
+    }
+    else
+    {
+      // Currency not valid for this server.
+      $restricted = TRUE;
     }
     return $restricted;
   }
